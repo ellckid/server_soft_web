1760cff72a0040d20198231010831387d1614f1d
Артем Лобов <artemlobov@192.168.1.14>
2022-10-25 01:11:08 +0300

    first commit

---
+++ dtos/user-dto.js
@@ -0,0 +1,14 @@
+const { model } = require("mongoose");
+
+module.exports = class UserDto {
+    email;
+    id;
+    isActivated;
+
+    constructor() {
+        this.email = model.email;
+        this.id = model._id;
+        this.isActivated = model.isActivated;
+    }
+
+}
