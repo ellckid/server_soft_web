dfdb9f2031668827c985328d6a5d0fb2b316d403
Артем Лобов <artemlobov@192.168.1.14>
2022-10-25 20:38:26 +0300

    fixed userData

--- dtos/user-dto.js
+++ dtos/user-dto.js
@@ -1,14 +1,11 @@
-const { model } = require("mongoose");
-
 module.exports = class UserDto {
     email;
     id;
     isActivated;
 
-    constructor() {
+    constructor(model) {
         this.email = model.email;
         this.id = model._id;
         this.isActivated = model.isActivated;
     }
-
 }
--- service/user-service.js
+++ service/user-service.js
@@ -2,18 +2,17 @@ const UserModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('./mail-service');
-const TokenService = require('./token-service');
 const tokenService = require('./token-service');
 const UserDto = require('../dtos/user-dto');
 
 class userService {
     async registration(email, password) {
-        const candidate = await UserModel.findOne({ email })
+        const candidate = await UserModel.findOne({ email });
         if (candidate) {
-            throw new Error('пользователь с такой почтой уже существует ! ')
+            throw new Error('пользователь с такой почтой уже существует ! ');
         }
         const hashPassword = await bcrypt.hash(password, 3);
-        const activationLink = uuid.v4;
+        const activationLink = uuid.v4();
 
         const user = await UserModel.create({ email, password: hashPassword, activationLink });
         await mailService.sendActivationMail(email, activationLink);
