981a3eb23108e7bd886105ab97cffedaa557a46c
Артем Лобов <artemlobov@192.168.1.14>
2022-10-26 22:25:55 +0300

    auth body ready

--- service/mail-service.js
+++ service/mail-service.js
@@ -1,6 +1,32 @@
-class MailService {
-    async sendActivationMail(to, link) {
+const nodemailer = require('nodemailer');
 
+class MailService {
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            // host: 'smtp.gmail.com',
+            // port: 587,
+            // secure: false,
+            service: "gmail",
+            auth: {
+                user: 'soft.web.mail.ru@gmail.com',
+                pass: 'ldntcmradqmqbfjd'
+            }
+        })
+    }
+    async sendActivationMail(to, link) {
+        await this.transporter.sendMail({
+            from: "soft.web.mail.ru@gmail.com",
+            to,
+            subject: "Подтверждение почты на сайте " + process.env.API_URL,
+            text: "",
+            html:
+                `
+                <div>
+                    <h1>Перейдите по ссылке для подтверждения аккаунта  </h1>
+                    <a href=${link}> ${link}</a>
+                </div>
+                `
+        })
     }
 }
 
--- service/user-service.js
+++ service/user-service.js
@@ -4,18 +4,20 @@ const uuid = require('uuid');
 const mailService = require('./mail-service');
 const tokenService = require('./token-service');
 const UserDto = require('../dtos/user-dto');
+const ApiError = require('../exceptions/api-error');
+const userModel = require('../models/user-model');
 
 class userService {
     async registration(email, password) {
         const candidate = await UserModel.findOne({ email });
         if (candidate) {
-            throw new Error('пользователь с такой почтой уже существует ! ');
+            throw ApiError.BadRequest('пользователь с такой почтой уже существует ! ');
         }
         const hashPassword = await bcrypt.hash(password, 3);
         const activationLink = uuid.v4();
 
         const user = await UserModel.create({ email, password: hashPassword, activationLink });
-        await mailService.sendActivationMail(email, activationLink);
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
 
         const userDto = new UserDto(user);
         const tokens = tokenService.generateTokens({ ...userDto });
@@ -25,7 +27,50 @@ class userService {
             ...tokens,
             user: userDto
         }
+    }
+    async activate(activationLink) {
+        const user = await UserModel.findOne({ activationLink });
+        if (!user) {
+            throw ApiError.BadRequest('Неправильная ссылка активации ( ');
+        }
+        user.isActivated = true;
+        await user.save();
+    }
+    async login(email, password) {
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            throw ApiError.BadRequest('Пользователь с таким email не найден (');
+        }
+        const isPassEquals = await bcrypt.compare(password, user.password);
+        if (!isPassEquals) {
+            throw ApiError.BadRequest('Неправильный пароль (');
+        }
 
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({ ...userDto });
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+        return { ...tokens, user: userDto };
+    }
+    async logout(refreshToken) {
+        const token = await tokenService.removeToken(refreshToken);
+        return token;
+    }
+    async refresh(refreshToken) {
+        if (!refreshToken) {
+            throw ApiError.UnauthorizedError();
+        }
+        const userData = tokenService.validateRefreshToken(refreshToken);
+        const tokenFromDb = await tokenService.findToken(refreshToken);
+        if (!userData || !tokenFromDb) {
+            throw ApiError.UnauthorizedError();
+        }
+        const user = await UserModel.findById(userData.id);
+        const userDto = new UserDto(user);
+        const tokens = tokenService.generateTokens({ ...userDto });
+
+        await tokenService.saveToken(userDto.id, tokens.refreshToken);
+        return { ...tokens, user: userDto };
     }
 }
 
